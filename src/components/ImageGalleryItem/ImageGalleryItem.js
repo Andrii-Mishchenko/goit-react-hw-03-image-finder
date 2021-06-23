37ff1a140985343d4d4fb030b7dbf540258ebaf2
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-06-23 14:32:07 +0300

    HomeWork for assessment

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../../styles.module.css'
 
-const ImageGalleryItem = ({ webformatURL, onClick }) => (
-    <li className={styles.ImageGalleryItem} onClick={onClick}>
+const ImageGalleryItem = ({webformatURL, id, onClick}) => (
+    <li key={id} className={styles.ImageGalleryItem} onClick={onClick} >
         <img src={webformatURL} alt="" className={styles.ImageGalleryItemImage} />
     </li>   
 )
